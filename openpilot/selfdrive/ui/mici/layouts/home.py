423f7983040e2486b3dffef0f24a196e09dd16bd
Patrick <pdemers@patdemers.com>
2026-08-25 13:49:59 -0400

    Replace mvl-boston buymeacoffee branding with a one-liner
    Home screen and startup alert both hardcoded "buymeacoffee.com/mvlboston"
    -- fine for their fork, not applicable on a personal tuning build. Swapped
    both for a joke instead of a donation link.

--- openpilot/selfdrive/ui/mici/layouts/home.py
+++ openpilot/selfdrive/ui/mici/layouts/home.py
@@ -203,7 +203,7 @@ class MiciHomeLayout(Widget):
     version = ui_state.params.get("Version")
     git_branch = ui_state.params.get("GitBranch")
     git_commit = ui_state.params.get("GitCommit")
-    commit = "https://buymeacoffee.com/mvlboston"
+    commit = "professionally unqualified since 2026"
 
     if not all((version, git_branch, git_commit, commit)):
       return None
